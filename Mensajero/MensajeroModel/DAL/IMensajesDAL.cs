c4e5f54388fcd2931f6da23ec1ea2f0185113516
francisco22corvalan@gmail.com <francisco22corvalan@gmail.com>
2021-05-06 01:49:47 -0400

    Actualizacion
    Generar los metodos del DAL y DTO

---
+++ Mensajero/MensajeroModel/DAL/IMensajesDAL.cs
@@ -0,0 +1,17 @@
+﻿using MensajeroModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajeroModel.DAL
+{
+    //INTERFAZ
+    public interface IMensajesDAL
+    {
+        void Save(Mensaje m);
+
+        List<Mensaje> GetAll();
+    }
+}
---
+++ Mensajero/MensajeroModel/DAL/MensajesDALArchivos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MensajeroModel.DTO;
+
+namespace MensajeroModel.DAL
+{
+    public class MensajesDALArchivos : IMensajesDAL
+    {
+        // Path.DirectorySeparatorChar remplaza el / (slash)
+        private string archivo = Directory.GetCurrentDirectory()
+            + Path.DirectorySeparatorChar + "mensajes.csv";
+
+        public List<Mensaje> GetAll()
+        {
+            List<Mensaje> mensajes = new List<Mensaje>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(archivo))
+                {
+                    string texto = null;
+                    do
+                    {
+                        texto = reader.ReadLine();
+                        if (texto != null)
+                        {
+                            //Parsear el mensaje
+                            string[] textoArray = texto.Split(';');
+                            Mensaje m = new Mensaje()
+                            {
+                                Nombre = textoArray[0],
+                                Detalle = textoArray[1],
+                                Tipo = textoArray[2]
+                            };
+                            mensajes.Add(m);
+                        }
+                    } while (texto != null);
+                }
+            }
+            catch (IOException ex)
+            {
+
+                mensajes = null;
+            }
+            return mensajes;
+        }
+
+
+        public void Save(Mensaje m)
+        {
+            try
+            {
+                using(StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(m);
+                    writer.Flush();
+                }
+            }
+            catch (IOException ex)
+            {
+
+            }
+        }
+    }
+}
