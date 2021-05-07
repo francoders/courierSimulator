6cb15978828a9b15fba4f314443060825dbc546d
francisco22corvalan@gmail.com <francisco22corvalan@gmail.com>
2021-05-06 21:28:36 -0400

    Actualizacion
    Final del proyecto
    - realiza conecion con el Server ademas de utilizar Hilos

---
+++ Mensajero/Mensajero/Hilos/HiloCliente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MensajeroModel.DAL;
+using MensajeroModel.DTO;
+using SocketUtils;
+
+namespace Mensajero.Hilos
+{
+    public class HiloCliente
+    {
+        private ClienteSocket clienteSocket;
+        private IMensajesDAL dal = MensajesDALFactory.CreateDal();
+
+        public HiloCliente(ClienteSocket clienteSocket)
+        {
+            this.clienteSocket = clienteSocket;
+        }
+
+
+        public void Ejecutar()
+        {
+            string nombre, detalle;
+            do
+            {
+                clienteSocket.Escribir("Ingrese Nombre: ");
+                nombre = clienteSocket.Leer().Trim();
+
+            } while (nombre == string.Empty);
+
+            do
+            {
+                clienteSocket.Escribir("Ingrese Mensaje: ");
+                detalle = clienteSocket.Leer().Trim();
+
+            } while (detalle == string.Empty || detalle.Length > 20);
+
+            Mensaje m = new Mensaje()
+            {
+                Nombre = nombre,
+                Detalle = detalle,
+                Tipo = "TCP"
+            };
+            dal.Save(m);
+            clienteSocket.CerrarConexion();
+        }
+
+    }
+}
--- Mensajero/Mensajero/Program.cs
+++ Mensajero/Mensajero/Program.cs
@@ -1,79 +1,29 @@
-﻿using MensajeroModel.DAL;
+﻿using Mensajero.Hilos;
+using MensajeroModel.DAL;
 using MensajeroModel.DTO;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Mensajero
+namespace Mensajero.Partials
 {
-    class Program
+    partial class Program
     {
-        //1. Crear Menu
-        //2. Dos metodos IngresarMensajes y MostrarMensajes
-        //3. Al ingresar un mensjae definir el tipo como aplicacion
-
-       static IMensajesDAL dal = MensajesDALFactory.CreateDal();
-
-       static void IngresarMensaje()
-        {
-            string nombre, detalle;
-            do
-            {
-                Console.WriteLine("Ingrese Nombre: ");
-                nombre = Console.ReadLine().Trim();
-            } while (nombre == string.Empty);
-
-             do
-            {
-                Console.WriteLine("Ingrese Mensaje: ");
-                detalle = Console.ReadLine().Trim();
-            } while (detalle == string.Empty || detalle.Length > 20);
-
-            Mensaje m = new Mensaje()
-            {
-                Nombre = nombre,
-                Detalle = detalle,
-                Tipo = "Aplicacion"
-            };
-            dal.Save(m);
-             
-
-        }
-
-        static void MostrarMensajes()
-        {
-            List<Mensaje> mensajes = dal.GetAll();
-            mensajes.ForEach(m =>
-            {
-                Console.Write("Nombre: {0} Detalle: {1} Tipo {2} \n", m.Nombre, m.Detalle, m.Tipo);
-            });
-
-        }
-
-        static bool Menu()
-        {
-            bool continuar = true;
-            Console.WriteLine("1. Ingrese Mensaje");
-            Console.WriteLine("2. Mostrar Mensaje");
-            Console.WriteLine("0. Salir");
-            string opcion = Console.ReadLine().Trim();
-            switch (opcion)
-            {
-                case "1": IngresarMensaje();
-                    break;
-                case "2": MostrarMensajes();
-                    break;
-                case "0": continuar = false;
-                    break;
-            }
-            return continuar;
-        }
 
         static void Main(string[] args)
         {
-            while (Menu()) ;
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Hilo del Server Socket");
+            HiloServer hiloServer = new HiloServer(puerto);
+            Thread t = new Thread(new ThreadStart(hiloServer.Ejecutar));
+            t.Start();
+            t.IsBackground = true;
+
+            while (Menu());
         }
     }
 }
